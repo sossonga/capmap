bc8c8d234d6841ed20cdf5fd6756ecd8cfde61b1
Mandy Sossong <sossonga1@gmail.com>
2019-04-18 06:45:55 -0400

    Sort statistics, add MAC addresses, fix comments

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # This script analyzes pcaps that have been captured and provides a visualization and statistics
-# Shows active IPs on a subnet, how many DNS packets have been transmitted, who hosts are talking to, MAC addresses
-# and more
+# Shows active IPs on a subnet, how many DNS packets have been transmitted, who hosts are talking to, MAC addresses, etc
 
 import scapy.all
 import argparse
@@ -30,56 +29,59 @@ def _args():
 def _parse_pcap(pcap):
     """The _parse_pcap function uses the scapy library to parse through a pcap for network data
     :param pcap: A raw pcap that needs to be parsed
-    :return Nothing:"""
+    :return: None"""
     print("Parsing " + str(pcap) + "...")
     # read pcap file
     pkt = scapy.all.rdpcap(pcap)
 
     # list initializations
-    src_ip_list = []
-    src_mac_list = []
-    src_port_list = []
+    master_ip_list = []
+    master_mac_list = []
+    master_port_list = []
     dns_query_list = []
     transport_list = []
     arp_list = []
-    transmissions_list = []
+    convo_list = []
 
     for packet in pkt:
         # if this is a layer 3 packet
         if IP in packet:
             src = packet['IP.src']
             dst = packet['IP.dst']
-            # transport layer is TCP
+            mac_src = packet['Ether.src']
+            mac_dst = packet['Ether.dst']
+            master_mac_list.extend([mac_src, mac_dst])
+            # if transport layer is TCP
             if TCP in packet:
                 # append TCP to list of transport protocols
                 transport_list.append('TCP')
                 # append string of source -> destination to list of conversations
                 packet_string = src + " -> " + dst
-                transmissions_list.append(packet_string)
+                convo_list.append(packet_string)
                 # append source and destination addresses to list of addresses
-                src_ip_list.extend([src, dst])
+                master_ip_list.extend([src, dst])
                 # append source and destination ports to list of ports
                 src_port = packet['TCP.sport']
                 dst_port = packet['TCP.dport']
-                src_port_list.extend([src_port, dst_port])
-            # transport layer is UDP
+                master_port_list.extend([src_port, dst_port])
+            # else if transport layer is UDP
             elif UDP in packet:
                 # append UDP to list of transport protocols
                 transport_list.append('UDP')
                 # append string of source -> destination to list of conversations
                 packet_string = src + " -> " + dst
-                transmissions_list.append(packet_string)
+                convo_list.append(packet_string)
                 # append source and destination addresses to list of addresses
-                src_ip_list.extend([src, dst])
+                master_ip_list.extend([src, dst])
                 # append source and destination ports to list of ports
                 src_port = packet['UDP.sport']
                 dst_port = packet['UDP.dport']
-                src_port_list.extend([src_port, dst_port])
-                # packet is has DNS
+                master_port_list.extend([src_port, dst_port])
+                # if packet has DNS
                 if DNS in packet:
                     # initialize list
                     dns_query = []
-                    # packet has a DNS query
+                    # if packet has a DNS query
                     if packet.haslayer(DNSQR):
                         # cast name of query to a string
                         query = packet['DNSQR.qname']
@@ -87,12 +89,12 @@ def _parse_pcap(pcap):
                     # append source address, source port, query, destination address, and destination port to dns list
                     dns_query.extend((src, src_port, str_query, dst, dst_port))
                     dns_query_list.append(dns_query)
-            # ICMP is in packet
+            # if ICMP is in packet
             elif ICMP in packet:
                 # append ICMP to list of transport protocols
                 transport_list.append('ICMP')
-                icmp_type = packet['ICMP.type']
-        # if this is a layer 2 packet
+                # icmp_type = packet['ICMP.type']
+        # else if this is a layer 2 packet
         elif ARP in packet:
             # initialize list
             arp_packet = []
@@ -104,25 +106,23 @@ def _parse_pcap(pcap):
             arp_list.append(arp_packet)
 
     # split master ip list into a list of source IPs
-    src_ips = src_ip_list[::2]
+    src_ips = master_ip_list[::2]
     # split master ip list into a list of destination IPs
-    dst_ips = src_ip_list[1::2]
-    # split master MAC list into a list of source MACs
-    src_macs = src_mac_list[::2]
-    # split master MAC list into a list of source MACs
-    dst_macs = src_mac_list[1::2]
+    dst_ips = master_ip_list[1::2]
 
     print("DONE")
 
-    statistics(src_ips, dst_ips, transmissions_list, src_port_list, transport_list, dns_query_list, arp_list)
+    statistics(src_ips, dst_ips, convo_list, master_mac_list, master_port_list, transport_list, dns_query_list,
+               arp_list)
 
 
-def statistics(src_ips, dst_ips, transmissions, port_list, transport, dns_queries, arps):
+def statistics(src_ips, dst_ips, transmissions, mac_list, port_list, transport, dns_queries, arps):
     """The statistics function takes lists from a parsed pcap as input, analyzes the contents and prints a visual
     to a .svg file. A report is then written to an .html file and opened in the default web browser.
     :param src_ips: A list of source addresses from the pcap file
     :param dst_ips: A list of destination addresses
     :param transmissions: A list of source -> destination addresses
+    :param mac_list: A list of MAC addresses from the pcap file
     :param port_list: A list of ports from the pcap file
     :param transport: A list of transport protocols
     :param arps: A list of ARP requests and replies
@@ -131,37 +131,44 @@ def statistics(src_ips, dst_ips, transmissions, port_list, transport, dns_querie
     print("Calculating Statistics...")
     # count number of unique conversations
     packet_count = collections.Counter(transmissions)
+    packet_sorted = collections.Counter.most_common(packet_count)
     # count number of unique ports
     port_count = collections.Counter(port_list)
+    port_sorted = collections.Counter.most_common(port_count)
     # count number of unique transport protocols
     trans_count = collections.Counter(transport)
+    trans_sorted = collections.Counter.most_common(trans_count)
+    # count number of unique MAC addresses
+    mac_count = collections.Counter(mac_list)
+    mac_sorted = collections.Counter.most_common(mac_count)
 
     print("DONE\nVisualizing...")
 
     # create a graph for the network hosts
-    dot = Digraph(comment='Network Diagram', format='svg')
-    dot.attr('node', shape='square')
+    net_diagram = Digraph(comment='Network Diagram', format='svg')
+    net_diagram.attr('node', shape='square')
     # initialize list
     already_done = []
     # zip together source IPs and destination IPs
     for address_pair in zip(src_ips, dst_ips):
         if address_pair not in already_done:
             # create nodes for the source IP and destination IP
-            dot.node(address_pair[0])
-            dot.node(address_pair[1])
+            net_diagram.node(address_pair[0])
+            net_diagram.node(address_pair[1])
             # create an edge between the source and destination
-            dot.edge(address_pair[0], address_pair[1])
+            net_diagram.edge(address_pair[0], address_pair[1])
             # append to the list of matched hosts
             already_done.append(address_pair)
     # render a graph to the specified file
-    dot.render('graph-output/net-map')
+    net_diagram.render('graph-output/net-map')
 
     # load the template from the specified folder
     file_loader = FileSystemLoader('templates')
     env = Environment(loader=file_loader)
     template = env.get_template('capmap.html')
     # render the template and pass in lists for processing in the template
-    render = template.render(trans=trans_count, ports=port_count, dns=dns_queries, arp=arps, packets=packet_count)
+    render = template.render(trans=trans_sorted, ports=port_sorted, dns=dns_queries, arp=arps, packets=packet_sorted,
+                             macs=mac_sorted)
     # write the completed template to the specified .html file
     filename = os.path.abspath("html/index.html")
     with open(filename, 'w') as f:
